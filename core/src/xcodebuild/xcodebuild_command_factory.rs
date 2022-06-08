04bf280e40db4844a7470bfb724ba0cd27afe71d
Stefano Sonzogni <sonzo995@gmail.com>
2022-06-08 16:16:54 +0200

    add export and upload commands

--- core/src/xcodebuild/xcodebuild_command_factory.rs
+++ core/src/xcodebuild/xcodebuild_command_factory.rs
@@ -31,4 +31,54 @@ impl XcodebuildCommandFactory {
             .args(["clean", "archive"]);
         command
     }
+
+    pub fn build_export(
+        &self,
+        schema: &str,
+        archive_folder: &Path,
+    ) -> Command {
+        let mut command = Command::new("xcodebuild");
+        if self.dry_run {
+            command.arg("-dry-run");
+        }
+        let archive_name = format!("{}.xcarchive", schema);
+        let export_name = format!("{}.ipa", schema);
+        let mut archive_path_buf = archive_folder.to_path_buf();
+        let mut export_path_buf = archive_path_buf.clone();
+        archive_path_buf.push(&archive_name);
+        export_path_buf.push(&export_name);
+
+        command
+            .arg("-exportArchive")
+            .args(["-archivePath", &archive_path_buf.to_str().unwrap()])
+            .args(["-exportPath", &export_path_buf.to_str().unwrap()]);
+        command
+    }
+
+    pub fn build_upload(
+        &self,
+        schema: &str,
+        export_folder: &Path,
+        username: &str,
+        password: &str,
+    ) -> Command {
+        let mut command = Command::new("xcrun");
+        command
+            .arg("altool")
+            .arg("--upload-app");
+        
+        if self.dry_run {
+            command.arg("-dry-run");
+        }
+        
+        let export_name = format!("{}.ipa", schema);
+        let mut export_path_buf = export_folder.to_path_buf();
+        export_path_buf.push(&export_name);
+
+        command
+            .args(["-f", &export_path_buf.to_str().unwrap()])
+            .args(["-u", username])
+            .args(["-p", password]);
+        command
+    }
 }
