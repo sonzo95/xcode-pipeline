541c2f5215097a18c51a8ce6a35b92fc25fc7319
Stefano Sonzogni <stefano.sonzogni@mia-platform.eu>
2022-06-09 18:58:55 +0200

    update install script to remove previous installations

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,7 @@ cd "${0%/*}";
 cd ..;
 echo "Installing from project directory $PWD"
 cargo build --release &&
+rm /usr/local/bin/xccd
 mv target/release/cli /usr/local/bin/xccd &&
 chmod +x /usr/local/bin/xccd &&
 echo "Successfully installed cli as 'xccd'"
